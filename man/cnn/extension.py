fdd95a993df1dfa0b0f69cb0d81a49a0581f8d88
suqi <deqi.su@gmail.com>
2017-06-21 11:28:15 +0800

    fix: state grid size

--- man/cnn/extension.py
+++ man/cnn/extension.py
@@ -1,4 +1,4 @@
-from gymgame.engine import Vector2
+from gymgame.engine import Vector2, Vector3
 from gymgame.tinyrpg.man import EnvironmentGym, Game, config
 
 # env constant
@@ -11,6 +11,9 @@ def _reward(self):
     return 1
 
 # state transform
-_state_grid_size = Vector2(int(config.MAP_SIZE.x/ config.PLAYER_RADIUS/ 2),
-                           int(config.MAP_SIZE.y / config.PLAYER_RADIUS / 2))
+_state_grid_shape = Vector3(
+    int(config.MAP_SIZE.x/ config.PLAYER_RADIUS/ 2),
+    int(config.MAP_SIZE.y / config.PLAYER_RADIUS / 2),
+    3*2  # (count, direction, speed)  of all players/npcs
+)
 
