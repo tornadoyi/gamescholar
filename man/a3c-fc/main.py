7fe05a32b9894a4d65ceae4ec6c22d7af69506ec
suqi <deqi.su@gmail.com>
2017-06-21 16:58:17 +0800

    fix: dir merge

--- man/a3c-fc/main.py
+++ man/a3c-fc/main.py
@@ -1,10 +1,16 @@
+"""
+flat-a3c
+A3C with a flattened 1D array state (with all info of all objects)
+"""
+
 import tensorflow as tf
 import threading
 import gym
 import os
 import shutil
 from .extension import config
-from .a3c import Worker, ACNet
+from .ac_net import ACNet
+from .ac_worker import ACWorker
 
 
 LR_A = 0.001  # learning rate for actor
@@ -39,7 +45,7 @@ def run():
         i_name = 'W_%i' % i  # worker name
         env = gym.make(config.GAME)
         ac = ACNet(sess, i_name, N_S, N_A, OPT_A, global_ac=GLOBAL_AC, entropy_beta=ENTROPY_BETA)
-        workers.append(Worker(ac, env, GAMMA))
+        workers.append(ACWorker(ac, env, GAMMA))
 
 
     COORD = tf.train.Coordinator()
