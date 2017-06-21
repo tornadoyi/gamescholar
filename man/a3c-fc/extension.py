01ecaf35e5745547ab7ed8b06fc9e7594230f9f6
tornadoyi <390512308@qq.com>
2017-06-21 17:46:15 +0800

    fix raycast

--- man/a3c-fc/extension.py
+++ man/a3c-fc/extension.py
@@ -3,26 +3,47 @@ from gymgame.engine import Vector2, Vector3, extension
 from gymgame.tinyrpg import man
 
 config = man.config
-
+Attr = config.Attr
 
 # env constant
 config.NUM_NPC = 1
 config.MAP_SIZE = Vector2(30, 30)
 
+def alive_object_count(list):
+    count = 0
+    for o in list:
+        if o.attribute.hp < 1e-6: continue
+        count += 1
+    return count
+
+@extension(man.NPC)
+class NPCExtension():
+    def _update(self):
+        if self.attribute.hp < 1e-6: return
+        self.move_toward(self.attribute.direct)
+        if self.attribute.hp < 1e-6 or not self._map.in_bounds(self.attribute.position):
+            self.attribute.hp = 0
+            self.attribute.position = config.MAP_SIZE
+            self.attribute.direct = Vector2.zero
+
+
+@extension(man.game)
+class GameExtension():
+    def _check_terminal(self):
+        player_alive = alive_object_count(self._map.players)
+        npc_alive = alive_object_count(self._map.npcs)
+        return (player_alive == 0) or (npc_alive == 0)
 
 
 @extension(man.EnvironmentGym)
 class EnvironmentExtension():
 
     @property
-    def player(self): return self.game.players[0].attribute.hp
-
-    @property
-    def npcs(self): return self.game.npcs
+    def player(self): return self.game.map.players[0]
 
     def _reset(self):
-        man.EnvironmentGym._reset(self)
-        self.npc_count = len(self.npcs)
+        super(man.EnvironmentGym, self)._reset()
+        self.npc_count = alive_object_count(self.game.map.npcs)
         self.player_hp = self.player.attribute.hp
 
 
@@ -33,15 +54,15 @@ class EnvironmentExtension():
 
     def _reward(self):
         def _sync():
-            self.npc_count = len(self.npcs)
+            self.npc_count = alive_object_count(self.game.map.npcs)
             self.player_hp = self.player.attribute.hp
 
-        sub_hp = self.player.attribute.hp - self.player_hp
+        sub_hp = self.player_hp - self.player.attribute.hp
         if sub_hp > 0:
             _sync()
             return -sub_hp / 10.0
 
-        sub_npc = len(self.npcs) - self.npc_count
+        sub_npc = self.npc_count - alive_object_count(self.game.map.npcs)
         if sub_npc > 0:
             _sync()
             return sub_npc / 10.0
@@ -51,23 +72,38 @@ class EnvironmentExtension():
 
 @extension(man.Serializer)
 class SerializerExtension():
-    def serialize_state(self, game):
-        pass
+
+    #def serialize_state(self, game):
+        #man.Serializer.serialize_state(self, game)
+
 
     def _deserialize_action(self, data):
         # data 1x3 (x, y, speed)
         direct = Vector2(data[0], data[1])
-        speed = data[3]
+        speed = data[2]
         actions = [(config.PLAYER_IDS[0], config.Action.move_toward, direct, speed)]
         return actions
 
 
+    '''
+    def _select_character(self, k):
+        self._select_object(k)
+        k.add(Attr.hp, None, k.n_div_tag(Attr.hp))
+    '''
 
 
-# state transform
-_state_grid_shape = Vector3(
-    int(config.MAP_SIZE.x/ config.PLAYER_RADIUS/ 2),
-    int(config.MAP_SIZE.y / config.PLAYER_RADIUS / 2),
-    3*2  # (count, direction, speed)  of all players/npcs
-)
+def run_game(render=False):
+    import gym
+    import time
+    env = gym.make(config.GAME_NAME)
+    env.reset()
 
+    while True:
+        if env.env.terminal: env.reset()
+        time.sleep(1.0 / 60)
+        env.step([-1, 1, 1])
+        if render: env.render()
+
+
+if __name__ == "__main__":
+    run_game()
